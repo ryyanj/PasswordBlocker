04c795e59b80ccf64f8e1df01df64caf66d5264d
ryyan <ryyanj@gmail.com>
2017-02-25 13:30:46 -0500

    added error checking for file writes

--- Utilities/WriteToFile.py
+++ Utilities/WriteToFile.py
@@ -3,9 +3,15 @@ import io
 import os.path
 
 def writePasswordToFile(password):
-	with io.open(os.path.abspath(os.pardir) + '/passwords.txt', 'a', encoding='utf-8') as file:
-    		file.write(password + '\n\n')
+	try:
+		with io.open(os.path.abspath(os.pardir) + '/passwords.txt', 'a', encoding='utf-8') as file:
+				file.write(password + '\n\n')
+	except Exception as e:
+		print(e)
 
 def writeXboxPasswordToFile(password):
-	with io.open(os.path.abspath(os.pardir) + '/xboxpasswords.txt', 'a', encoding='utf-8') as file:
-    		file.write(password + '\n\n')
+	try:
+		with io.open(os.path.abspath(os.pardir) + '/xboxpasswords.txt', 'a', encoding='utf-8') as file:
+				file.write(password + '\n\n')
+	except Exception as e:
+		print(e)
