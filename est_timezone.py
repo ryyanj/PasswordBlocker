e6708014ba7553604e90e136214e6d24bbcc2387
rxs4498 <ryyan_smith@homedepot.com>
2017-05-14 00:50:40 -0400

    update

--- est_timezone.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-class EST(datetime.tzinfo):
-    def utcoffset(self, dt):
-      return datetime.timedelta(hours=-5)
-
-    def dst(self, dt):
-        return datetime.timedelta(0)
--- pill_scheduler.py
+++ pill_scheduler.py
@@ -1,15 +1,15 @@
 import atexit
 import logging
+from pytz import timezone
+import pytz
 from twilio.rest import Client
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
-from est_timezone import EST
-
-
 
 logging.basicConfig()
-
+eastern = timezone('US/Eastern')
+utc = pytz.utc
 # Find these values at https://twilio.com/user/account
 account_sid = "ACdc0bae8c0927f2fc28fb18d90d742832"
 auth_token = "07b890944fba14516cdae417f4b9e4fb"
@@ -17,12 +17,12 @@ client = Client(account_sid, auth_token)
 scheduler = BackgroundScheduler()
 
 class PillScheduler():
-	est = EST()
+	
 	def pill_job(self):
 		scheduler.start()
 		scheduler.add_job(
 		func=self.print_pill_message,
-		trigger=CronTrigger(year='*', month='*', day='*', week='*', day_of_week='*', hour='*', minute='*', second='*',timezone=est),
+		trigger=CronTrigger(year='*', month='*', day='*', week='*', day_of_week='*', hour='*', minute='*', second='*',timezone=eastern),
 		id='printing_job',
 		name='Print pill message every day at 11:30 AM Eastern Time',
 		replace_existing=True)
