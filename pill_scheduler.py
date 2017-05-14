c9b7bf890c2e166259e9837f5a87e80f7157ba6d
rxs4498 <ryyan_smith@homedepot.com>
2017-05-14 01:17:02 -0400

    update

--- pill_scheduler.py
+++ pill_scheduler.py
@@ -21,7 +21,7 @@ class PillScheduler():
 		scheduler.start()
 		scheduler.add_job(
 		func=self.print_pill_message,
-		trigger=CronTrigger(year='*', month='*', day='*', week='*', day_of_week='*', hour='1', minute='16', second='40'),
+		trigger=CronTrigger(year='*', month='*', day='*', week='*', day_of_week='*', hour='1', minute='17', second='30'),
 		id='printing_job',
 		name='Print pill message every day at 11:30 AM Eastern Time',
 		replace_existing=True)
