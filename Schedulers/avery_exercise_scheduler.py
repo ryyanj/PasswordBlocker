63e18a9efa5a0bdd12e812432871ef564fa9fbaf
Ryyan Smith <ryyansmith@lawn-128-61-79-16.lawn.gatech.edu>
2017-11-15 14:09:09 -0500

    update

--- Schedulers/avery_exercise_scheduler.py
+++ Schedulers/avery_exercise_scheduler.py
@@ -9,7 +9,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 
 logging.basicConfig()
-utc = pytz.utc
+est = pytz.est
 # Find these values at https://twilio.com/user/account
 account_sid = "ACdc0bae8c0927f2fc28fb18d90d742832"
 auth_token = "07b890944fba14516cdae417f4b9e4fb"
@@ -21,7 +21,7 @@ class AveryExerciseScheduler():
 		scheduler.start()
 		scheduler.add_job(
 		func=self.print_avery_exercise_job,
-		trigger=CronTrigger(year='*', month='*', day='*', week='*', day_of_week='*', hour='11-22', minute='22', second='00',timezone=utc),
+		trigger=CronTrigger(year='*', month='*', day='*', week='*', day_of_week='*', hour='11-22', minute='22', second='00',timezone=est),
 		id='printing_job',
 		name='Print exercise schedule message every day on the 22nd minute of each hour between 11am and 10pm Eastern Time',
 		replace_existing=True)
