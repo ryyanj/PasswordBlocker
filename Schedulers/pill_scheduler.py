388833764c266949f5d49c7b56600736ecb41d8e
Ryyan Smith <ryyanj@gmail.com>
2017-11-16 16:49:51 -0500

    updated pill scheduler to eastern time zone and correct time

--- Schedulers/pill_scheduler.py
+++ Schedulers/pill_scheduler.py
@@ -9,7 +9,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 
 logging.basicConfig()
-utc = pytz.utc
 # Find these values at https://twilio.com/user/account
 account_sid = "ACdc0bae8c0927f2fc28fb18d90d742832"
 auth_token = "07b890944fba14516cdae417f4b9e4fb"
@@ -21,7 +20,7 @@ class PillScheduler():
 		scheduler.start()
 		scheduler.add_job(
 		func=self.print_pill_message,
-		trigger=CronTrigger(year='*', month='*', day='*', week='*', day_of_week='*', hour='15', minute='30', second='00',timezone=utc),
+		trigger=CronTrigger(year='*', month='*', day='*', week='*', day_of_week='*', hour='11', minute='30', second='00',timezone('US/Eastern')),
 		id='printing_job',
 		name='Print pill message every day at 11:30 AM Eastern Time',
 		replace_existing=True)
