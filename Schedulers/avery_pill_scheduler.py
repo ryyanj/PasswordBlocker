83b269d2896d3b6e086e3021bac87ecb39b02a15
Ryyan Smith <ryyansmith@lawn-128-61-79-16.lawn.gatech.edu>
2017-11-17 18:19:03 -0500

    update

--- Schedulers/avery_pill_scheduler.py
+++ Schedulers/avery_pill_scheduler.py
@@ -23,7 +23,7 @@ class PillScheduler():
 		scheduler.start()
 		scheduler.add_job(
 		func=self.print_pill_message,
-		trigger=CronTrigger(year='*', month='*', day='*', week='*', day_of_week='*', hour='23', minute='11', second='*',timezone=utc),
+		trigger=CronTrigger(year='*', month='*', day='*', week='*', day_of_week='*', hour='23', minute='*', second='*',timezone=utc),
 		id='printing_job',
 		name='Print pill message every day at 11:30 AM Eastern Time',
 		replace_existing=True)
