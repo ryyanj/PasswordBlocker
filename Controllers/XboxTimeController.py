97ad76de17c519c5d90786982b4a01a0915dcd33
Ryyan Smith <ryyanj@gmail.com>
2018-03-11 12:54:41 -0400

    updated time message b/c i didnt add new feature

--- Controllers/XboxTimeController.py
+++ Controllers/XboxTimeController.py
@@ -65,7 +65,7 @@ class XboxTime():
 				timeram.updateStopTime(newStopTime)
 				return "Updated Time: " + self.getTimeLeftInMinutes() + "."
 			else:
-				return "updated time to: " + str(MAX_BLOCK_LIMIT)
+				return "time is at max limit of: " + str(MAX_BLOCK_LIMIT)
 		elif float(time) < 0:
 			return "This is not a valid entry"
 		else:
