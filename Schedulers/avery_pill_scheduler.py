6db8ee9bef82e72fb52e2a68e6e02a01996d8eb4
Ryyan Smith <ryyanj@gmail.com>
2018-03-11 12:32:55 -0400

    updated timer because daylight savings time began on march 10

--- Schedulers/avery_pill_scheduler.py
+++ Schedulers/avery_pill_scheduler.py
@@ -23,7 +23,11 @@ class AveryPillScheduler():
 		scheduler.start()
 		scheduler.add_job(
 		func=self.print_pill_message,
-		trigger=CronTrigger(year='*', month='*', day='*', week='*', day_of_week='*', hour='16', minute='30', second='00',timezone=utc),
+		#use this timer after daylight savings time ends on November 17
+		#trigger=CronTrigger(year='*', month='*', day='*', week='*', day_of_week='*', hour='16', minute='30', second='00',timezone=utc),
+		#id='printing_job',
+		#use this timer after daylight savings time begin on March 10
+		trigger=CronTrigger(year='*', month='*', day='*', week='*', day_of_week='*', hour='15', minute='30', second='00',timezone=utc),
 		id='printing_job',
 		name='Print pill message every day at 11:30 AM Eastern Time',
 		replace_existing=True)
